df6998bc049f0d066cbf3d669f613aa3a939362e
bea94k <beatakusnierz94@gmail.com>
2020-05-07 14:58:56 +0300

    Display orders data to helper homepage (through OrderCard component), add order data to dummy DB

---
+++ neighbors-help/src/Components/OrderCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const PostCard = ({ username, firstname, address, phone, order, link }) => {
+    console.log(order);
+    let i;
+    let extractedOrder = [];
+    for (i = 0; i < order.length; i++) {
+        let productSet = `${order[i].name} - ${order[i].quantity}`;
+        extractedOrder.push(productSet);
+    }
+
+    return (
+        <div>
+            <h2>{firstname}</h2>
+            <p>{username}</p>
+            <p>{address}</p>
+            <p>{phone}</p>
+            <p>{extractedOrder}</p>
+            {/*  <Link to={link}>Read more</Link> */}
+        </div>
+    );
+};
+
+export default PostCard;
--- neighbors-help/src/HomeHelper.js
+++ neighbors-help/src/HomeHelper.js
@@ -1,12 +1,42 @@
 // homepgae for helper
 // display list of orders from database
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import OrderCard from './Components/OrderCard';
 
 const HomeHelper = () => {
+    const [order, setOrder] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3005/orders')
+            .then((response) => {
+                const ordersFromDB = response.data.slice(0, 15);
+                setOrder(ordersFromDB);
+                console.log(ordersFromDB);
+            })
+    }, []);
+
+
+    const OrderList = order.map((o) => {
+        return (
+            <OrderCard
+                key={o.id}
+                username={o.username}
+                firstname={o.firstname}
+                address={o.address}
+                phone={o.phone}
+                order={o.order}
+            />
+        )
+    })
+
+
     return (
         <div>
             HOME HELPER
+            {OrderList}
         </div>
     );
 }
